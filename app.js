8e4c7a42213bcd6a92ca7e39c61a7a28a2ed0c04
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-28 10:45:32 +0100

    Grammar done

---
+++ app.js
@@ -0,0 +1,7 @@
+// Basic fucntions
+
+function nameOfFunction(parameter){
+    return "this parameter can be changed dynamically as long as it matches my word in between my parenthesis: "+parameter;
+}
+
+const storingInVariable = nameOfFunction("Hello World")
